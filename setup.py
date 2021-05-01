7a449c6329fbcfa3dce232e39694053cf90ffe1a
tarpalantiri <tehseensajjadsb@gmail.com>
2021-05-01 17:27:10 +0500

    migrate to package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="Unclutter",
+      version='1.0',
+      description="Program to organize project files",
+      author="Tehseen Sajjad",
+      url="https://github.com/tarpalantiri/unclutter",
+      license="GPL",
+      author_email="tehseensajjadsb@gmail.com",
+      packages=['prog'],
+      )
