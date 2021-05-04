e056a163b947d137cdbd2731ced4d1649c7f6658
tarpalantiri <tehseensajjadsb@gmail.com>
2021-05-04 14:23:32 +0500

    whitespace

--- unclutter.py
+++ unclutter.py
@@ -1,17 +1,12 @@
-#! python3
-#---------------------------------------------------------------------------------------------------
 # [Author]
 # Tehseen Sajjad
-#
 # [Summary]
 # A small personal script that unclutters my desktop when working on an assignment or lab project.
-#
 # [Usage/Requirements]
 # * Python > 3.5.x
-# * files need to have '-' charactes in their name.
+# * files need to have '-' characters in their name.
 # * The second word after the first '-' will be the folder to which the files will be moved.
 # * Names of the folders are changed to title cases.
-#---------------------------------------------------------------------------------------------------
 import lib
 
 if __name__ == '__main__':
